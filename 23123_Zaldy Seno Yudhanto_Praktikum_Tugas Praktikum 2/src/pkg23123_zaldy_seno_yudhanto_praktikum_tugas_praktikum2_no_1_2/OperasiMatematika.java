27298a510d6fae78bf232d82b494114265423539
Dokii06 <zaldyseno06@gmail.com>
2024-09-08 21:14:23 +0700

    Mengupload tugas pertemuan 2 praktikum

---
+++ 23123_Zaldy Seno Yudhanto_Praktikum_Tugas Praktikum 2/src/pkg23123_zaldy_seno_yudhanto_praktikum_tugas_praktikum2_no_1_2/OperasiMatematika.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package pkg23123_zaldy_seno_yudhanto_praktikum_tugas_praktikum2_no_1_2;
+
+/**
+ *
+ * @author user
+ */
+public interface OperasiMatematika {
+    public abstract int pertambahan(int a, int b);
+    public abstract int pengurangan(int a, int b);
+    public abstract int perkalian(int a, int b);
+    public abstract int pembagian(int a, int b);
+}
---
+++ 23123_Zaldy Seno Yudhanto_Praktikum_Tugas Praktikum 2/src/pkg23123_zaldy_seno_yudhanto_praktikum_tugas_praktikum2_no_3/BingoKonstruktor.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg23123_zaldy_seno_yudhanto_praktikum_tugas_praktikum2_no_3;
+
+/**
+ *
+ * @author user
+ */
+public class BingoKonstruktor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Bingo song = new Bingo("Bingo");
+        
+        song.sing();
+    }
+}
